440a0e3b93bcaff116c2f38310a968d6f9c38554
TheSlipper <korneldomeradzki1@gmail.com>
2019-04-07 10:46:41 +0000

    Fixed my shit

---
+++ L07/74.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Argumenty: poczatkowa_miejsc, posr_miejsc, docelowa_miejsc, odl_AB, odl_BC
+full_length=`expr $4 + $5`
+filename=74.txt
+echo "" > 74.txt
+echo "Znaleziono trase: " >> $filename
+echo "Start: $1" >> $filename
+echo "Przez: $2" >> $filename
+echo "Meta: $3" >> $filename
+echo "Odleglosc: $full_length" >> $filename
